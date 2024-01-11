6d6c8057bf7f4c1969113d8399aef9c7f3518c69
caitlincorrigan <caitlinfcorrigan@gmail.com>
2024-01-11 11:34:33 -0800

    completes part 4 of tutorial

--- polls/urls.py
+++ polls/urls.py
@@ -4,9 +4,11 @@ from . import views
 # Add namespace with app name for projects with multiple apps
 app_name='polls'
 urlpatterns = [
-    path("", views.index, name="index"),
-    path('<int:question_id>/', views.details, name='detail'),
-    path('<int:question_id>/results/', views.results, name='results'),
+    # path("", views.index, name="index"),
+    # path('<int:question_id>/', views.details, name='detail'),
+    # path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
-
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,28 +1,31 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.views import generic
 # To avoid hardcoding the redirect url
 from django.urls import reverse
 
 from .models import Question, Choice
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    # create context var to hold object with req vars
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return render(request, 'polls/index.html', context)
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     # create context var to hold object with req vars
+#     context = {
+#         "latest_question_list": latest_question_list,
+#     }
+#     return render(request, 'polls/index.html', context)
 
-def details(request, question_id):
-    # Instead of try/except, use Django shortcut to throw 404 if resource (survey) does not exist
-    # First arg is Model, then kwargs
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {"question": question})
+# def details(request, question_id):
+#     # Instead of try/except, use Django shortcut to throw 404 if resource (survey) does not exist
+#     # First arg is Model, then kwargs
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {"question": question})
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {"question": question})
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {"question": question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -41,3 +44,20 @@ def vote(request, question_id):
     # Always return HttpResponseRedirect after successfully POST-ing to prevent data from posting twice if user hits Back button
     return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
     # Reverse url retruns string like /polls/3/results
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        # Return last 5 questions
+        return Question.objects.order_by('-pub_date')[:5]
+    
+class DetailView(generic.DetailView):
+    model = Question
+    # Default template name is <app name>/<model name>_detail.html
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
